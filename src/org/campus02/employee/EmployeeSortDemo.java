1c0cdd7ae777946c2eeb5609bfe510aa977529f2
Daniel Zettel <daniel.zettel@lv.campus02.at>
2025-03-08 08:49:51 +0100

    cart comparators

--- src/org/campus02/employee/EmployeeSortDemo.java
+++ src/org/campus02/employee/EmployeeSortDemo.java
@@ -10,12 +10,25 @@ public class EmployeeSortDemo {
         Employee[] employees = {
                 new Employee(5, "max", 3_000, "Development"),
                 new Employee(2, "susi", 6_000, "Research"),
+                new Employee(3, "mary", 5_000, "Research"),
                 new Employee(6, "john", 4_500, "Sales"),
                 new Employee(1, "max", 2_500, "Marketing")
         };
         System.out.println(Arrays.toString(employees));
 
         Arrays.sort(employees);
+        System.out.println(Arrays.toString(employees));
+
+        Arrays.sort(employees, new NameAscComparator());
+        System.out.println(Arrays.toString(employees));
+
+        Arrays.sort(employees, new SalaryDescComparator());
+        System.out.println(Arrays.toString(employees));
+
+        Arrays.sort(employees, new DepartmentAscNameDescComparator());
+        System.out.println(Arrays.toString(employees));
+
+        Arrays.sort(employees, new SalaryAscNameAscComparator());
         System.out.println(Arrays.toString(employees));
     }
 }
